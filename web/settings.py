1960554a1d93a8327942bc16fb21e87bdea12dc4
Oki4Doki4 <Oleg130601@mail.ru>
2022-01-19 23:05:08 +0500

    fix for deploy

--- web/settings.py
+++ web/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # мб будет ломаться - проверить при деплое!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://lit-tundra-85566.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['lit-tundra-85566.herokuapp.com','127.0.0.1']
 
 # Application definition
 
